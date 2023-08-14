9102f41b3cd9769c63d71cf05c8d0a8edcdc8e58
Vladislav Titar <titar-vlad@mail.ru>
2023-08-15 00:31:28 +0300

    di-annotation added

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.repository;
 
 import ru.netology.model.Post;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
+@Repository
 public class PostRepository {
     private final ConcurrentHashMap<Long, Post> repo = new ConcurrentHashMap<>();
     private final AtomicLong idCounter = new AtomicLong(1);
